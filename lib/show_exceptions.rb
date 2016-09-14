b06792a069ac89493090bad22eedc259c3c37612
Taylor Herron <taylorbherron@gmail.com>
2016-09-14 12:29:25 -0400

    Finish exceptions middleware

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -1,15 +1,25 @@
 require 'erb'
+require 'byebug'
 
 class ShowExceptions
+  attr_reader :app
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    begin
+      app.call(env)
+    rescue => e
+      ['500', {'Content-type' => 'text/html'}, ["#{render_exception(e)}"]]
+    end
   end
 
   private
 
   def render_exception(e)
+    contents = File.read("lib/templates/rescue.html.erb")
+    ERB.new(contents).result(binding)
   end
 
 end
